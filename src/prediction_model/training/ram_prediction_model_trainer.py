de0880067e023d894444771f2147fc8df2a637b1
thomaoc1 <73828133+thomaoc1@users.noreply.github.com>
2026-04-20 15:17:08 +0200

    RAM PM

---
+++ src/prediction_model/training/ram_prediction_model_trainer.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn.functional as F
+from torch.optim import Optimizer
+from torch.utils.data import DataLoader
+
+from .common import PredictionModelTrainer
+
+
+class RAMStatePredictionModelTrainer(PredictionModelTrainer):
+    def _iteration(
+        self,
+        loader: DataLoader,
+        teacher_forcing_prob: float,
+        optimizer: Optimizer | None = None,
+    ):
+        epoch_loss = 0.0
+        for current_state, action, next_state, _, _ in loader:
+            current_state, action_seq, next_state = (
+                current_state.to(self.device).float(),
+                action.to(self.device),
+                next_state.to(self.device),
+            )
+
+            action_seq = F.one_hot(action_seq, num_classes=self.n_actions).float()
+
+            _, K, _ = action_seq.shape
+            state_stack = current_state
+
+            predictions = []
+
+            hidden = None
+            for t in range(K):
+                action_t = action_seq[:, t, :]
+                pred_state, hidden = self.model(state_stack / 255.0, action_t, hidden)
+                predictions.append(pred_state)
+
+                if not optimizer or torch.rand(1).item() < teacher_forcing_prob:
+                    next_true_state = next_state[:, t, -1].float()
+                else:
+                    next_true_state = pred_state.argmax(dim=-1).float()
+
+                state_stack = next_true_state.unsqueeze(1)
+
+            predictions = torch.stack(predictions, dim=1)
+            n_classes = predictions.size(-1)
+            logits_flat = predictions.reshape(-1, n_classes)
+            target_flat = next_state[:, :, -1, :].reshape(-1)
+            loss = F.cross_entropy(logits_flat, target_flat, reduction="mean")
+
+            epoch_loss += loss.item()
+
+            if optimizer:
+                optimizer.zero_grad()
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1.0)
+                optimizer.step()
+
+        return epoch_loss / len(loader)
+
+    def train(
+        self, train_loader: DataLoader, val_loader: DataLoader, epochs: int = 100, lr: float = 0.03
+    ) -> tuple[list, list]:
+        optimizer = torch.optim.Adam(self.model.parameters())
+
+        for epoch in range(epochs):
+            teacher_forcing_prob = self.teacher_forcing_schedule(epoch, total_epochs=epochs)
+            self.model.train()
+            avg_train_epoch_loss = self._iteration(train_loader, teacher_forcing_prob, optimizer) 
+            self.train_losses.append(avg_train_epoch_loss)
+
+            self.model.eval()
+            avg_val_epoch_loss = self._iteration(val_loader, teacher_forcing_prob=0)
+            self.validation_losses.append(avg_val_epoch_loss)
+
+            print(
+                f"Epoch {epoch}: Train Loss = {avg_train_epoch_loss:.4f}, "
+                f"Val Loss = {avg_val_epoch_loss:.4f},"
+                f" Teacher forcing prob = {teacher_forcing_prob:.4f}"
+            )
+            self.save()
+
+        return self._get_results()
+
+    def save(self):
+        print(f"Saving model to: {self.prediction_model_path_builder.ram_model_weights()}")
+        torch.save(self.model.state_dict(), self.prediction_model_path_builder.ram_model_weights())
